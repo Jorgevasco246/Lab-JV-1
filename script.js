a58b67b38c5d7bfe83262a767aacf5e577487aca
Jorgevasco246 <1107844162@u.icesi.edu.co>
2025-04-04 18:10:44 -0500

    Labo

--- script.js
+++ script.js
@@ -42,15 +42,47 @@
 // console.log("Gracias por jugar");
 
 //2
-let frutas= ["manzana", "banano", "uva", "pera", "sandia", "kiwi", "naranja"]
-console.log(frutas.sort())
+let frutas = ['manzana', 'banano', 'uva', 'pera', 'sandia', 'kiwi', 'naranja'];
+console.log("Frutas:", frutas);
+
+frutas.sort();
+console.log("Frutas ordenadas:", frutas);
+
 
 function SingularAPlural(palabra) {
-    if (palabra.endsWith("s")){
-        return palabra
-    }
-    return palabra + "s"
-}   
-console.log(SingularAPlural(frutas[3]))
+  if (palabra.endsWith('s')) {
+    return palabra;
+  } else {
+    return palabra + 's';
+  }
+}
 
-for(palabra of )
+let frutasPlural = [];
+for (let fruta of frutas) {
+  frutasPlural.push(SingularAPlural(fruta));
+}
+console.log("Frutas en plural:", frutasPlural);
+
+
+function existeFruta(nombre) {
+  return frutas.includes(nombre);
+}
+console.log("¿Existe 'mango'?", existeFruta("mango")); 
+console.log("¿Existe 'uva'?", existeFruta("uva")); 
+
+
+let frutasCortas = frutas.filter(fruta => fruta.length <= 4);
+console.log("Frutas cortas (≤ 4 letras):", frutasCortas);
+
+function eliminarFruta(nombre) {
+  let ft = frutas.indexOf(nombre);
+  if (ft !== -1) {
+    frutas.splice(ft, 1);
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log("Eliminar 'pera':", eliminarFruta("pera"));
+console.log("Eliminar 'mango':", eliminarFruta("mango")); 
+console.log("Frutas después de eliminar:", frutas);
